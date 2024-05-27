649ef1bc404ad7189cc728061457b74498d459cb
Patro <jawaharlal.patro@randstadusa.com>
2024-05-27 18:30:38 +0530

    added docker and docker-compose files

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 #configuration for mongoDB and for JWT authentication
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    MONGO_URI = os.getenv('MONGO_URI')
+    MONGO_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
